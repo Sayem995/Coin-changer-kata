abd5f943b10d31c5f2cfe1d5ea3a38c6884b8a6c
Sayem995 <sayem.bokhari.1@gmail.com>
2020-02-17 15:22:56 +0000

    wrote second failing test

--- spec/changer_spec.rb
+++ spec/changer_spec.rb
@@ -1,7 +1,13 @@
 require 'changer'
 
 describe Changer do
+
   it 'has a change method' do
     expect(Changer.new.change).to eq(nil)
   end
+
+  it 'returns 1 penny for 1 penny' do
+    expect(Changer.new.change(1)).to eq([1])
+  end
+
 end
